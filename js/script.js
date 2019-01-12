3a630ff14f3e7f3447f65dfa844f39f15852bb6e
Frontendman25 <designer24@i.ua>
2019-01-12 18:17:10 +0200

    fourth commit

--- js/script.js
+++ js/script.js
@@ -10,9 +10,15 @@ $(function () {
     dots: false,
     prevArrow: '<img class="slider__arrow slider__arrow_left" src="img/arrow-left.jpg" />',
     nextArrow: '<img class="slider__arrow slider__arrow_right" src="img/arrow-right.jpg" />',
-    //    autoplay: true,
-    //    autoplaySpeed: 4000
+    autoplay: true,
+    autoplaySpeed: 4000
   });
+
+  // Addin styles for centering 
+  $('.slider__img-wrap').css({
+    "display": "flex",
+    "align-tems": "center"
+  })
   // --- Slider end --- \\
 
 
@@ -27,11 +33,11 @@ $(function () {
   span.onclick = function () {
     modal.style.display = "none";
   }
-  //window.onload = function () {
-  //  setTimeout(function () {
-  //    modal.style.display = 'block';
-  //  }, 3000);
-  //}
+  //  window.onload = function () {
+  //    setTimeout(function () {
+  //      modal.style.display = 'block';
+  //    }, 3000);
+  //  }
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
@@ -45,17 +51,31 @@ $(function () {
   $("#burger-menu").on("click", function (e) {
     $('.nav__item').stop().slideToggle('400')
   });
-
   // --- Toggle menu end --- \\
 
+
+
+  // --- Phones toggle --- \\
   $('.fa-phone-volume').on('click', function (e) {
     $('.feedback__wrap-phones').stop().slideToggle('400')
   })
+  // --- Phones toggle end --- \\
 
+
+
+  // --- Settings for slideToggle --- \\
   $(window).resize(function () {
     if ($(window).width() > 768 || $(window).width() > 576) {
       $('.nav__item').removeAttr('style');
       $('.feedback__wrap-phones').removeAttr('style');
     }
   })
+  // --- Settings for slideToggle end --- \\
+
+
+
+  // --- Footer to bottom --- \\
+  if ($('html').height() < $(window).height()) {
+    $(".footer-wrap").addClass("fixed-bottom");
+  }
 })
